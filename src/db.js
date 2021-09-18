f0ec8c8ce4afadb276cdf8f8fad3ae9021e091b0
kamal nayak <kamal.nayak@triggybet.com>
2021-09-18 16:41:06 +0200

    resolve issue

---
+++ src/db.js
@@ -0,0 +1,11 @@
+const github = {
+  baseURL: "https://api.github.com/graphql",
+  username:"john5037",
+
+  headers: {
+    "Content-Type": "application/json",
+    Authorization:"bearer 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"
+  }
+}
+
+export default github;
